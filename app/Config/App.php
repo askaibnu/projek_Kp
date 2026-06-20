e9f51f6b58d3b8a49648343a3826413182136070
askaibnu <ibnuaska867@gmail.com>
2026-06-20 22:28:58 +0700

    Fix mengubah baseURL menjadi domain Vercel agar aset termuat

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-public string $baseURL = 'https://projek-kqo7c0osa-ibnuaska.vercel.app/';
+public string $baseURL = '';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
