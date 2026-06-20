ba72fff84f6aeb1aa95b311aa05c8bd30590a124
askaibnu <ibnuaska867@gmail.com>
2026-06-20 20:35:34 +0700

    Fix menambahkan properti sensitiveDataInTrace

--- app/Config/Exceptions.php
+++ app/Config/Exceptions.php
@@ -6,56 +6,20 @@ use CodeIgniter\Config\BaseConfig;
 
 class Exceptions extends BaseConfig
 {
-    /**
-     * --------------------------------------------------------------------------
-     * LOG EXCEPTIONS
-     * --------------------------------------------------------------------------
-     * Define whether an exception should be logged or not.
-     */
     public bool $logDeprecations = true;
-
-    /**
-     * --------------------------------------------------------------------------
-     * LOG NOTICES
-     * --------------------------------------------------------------------------
-     * Define whether an exception should be logged or not.
-     */
     public bool $logNotices = true;
-
-    /**
-     * --------------------------------------------------------------------------
-     * DO NOT LOG EXCEPTIONS
-     * --------------------------------------------------------------------------
-     * DO NOT log these Exception classes.
-     *
-     * @var list<class-string<\Throwable>>
-     */
     public array $ignoreCodes = [
         \CodeIgniter\Router\Exceptions\RedirectException::class,
     ];
-
-    /**
-     * --------------------------------------------------------------------------
-     * Error Views Path
-     * --------------------------------------------------------------------------
-     * The path to the directory containing the views used to display errors.
-     */
     public string $errorViewPath = APPPATH . 'Views/errors';
-
-    /**
-     * --------------------------------------------------------------------------
-     * LOG PATH
-     * --------------------------------------------------------------------------
-     * The path to the directory where logs should be stored.
-     */
     public string $logPath = '/tmp/';
+    public string $log = 'log_exceptions';
 
     /**
      * --------------------------------------------------------------------------
-     * LOGGING GROUP
+     * SENSITIVE DATA IN TRACE
      * --------------------------------------------------------------------------
-     * This is the name of the log group that should be used for logging.
-     * (Variabel ini yang tadi sempat terlewat, Nu)
+     * Ini variabel baru yang diminta oleh CodeIgniter versi 4.7 ke atas, Nu.
      */
-    public string $log = 'log_exceptions';
+    public array $sensitiveDataInTrace = [];
 }
